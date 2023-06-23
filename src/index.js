75b56557fc157b48b59210b543597f2cc73e8bf4
emma wambui <emma.wambui@student.moringaschool.com>
2023-06-23 15:36:56 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
+//document.addEventListener("DOMContentLoaded", () => {
+  //document.querySelector("#create-task-form").addEventListener('CreateNewTask', (e) => {
+    //e.preventDefault()
+    //buildToDo(e.target.newToDo.value)
+  //})
+//});
+
+//function buildToDo(newToDo){
+ // let p = document.createElement('p');
+  //let btn = document.createElement('button')
+  //btn.textContent = 'cancel'
+  //p.textContent = `${newToDo}`
+  //p.appendChild(btn)
+  //console.log(p)
+  //document.querySelector('#create-task-form').appendChild(p)
+//}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formNode = document.querySelector("#create-task-form");
+  formNode.addEventListener("submit", buildTaskLister);
 });
+
+const buildTaskLister = (e) => {
+  e.preventDefault();
+  const inputNode = document.querySelector("#new-task-description");
+  const tasksNode = document.querySelector("#tasks");
+
+  const buttonNode = createButton();
+  const listNode = createLI(inputNode.value, buttonNode);
+  tasksNode.append(listNode);
+};
+
+const deleteTask = (e) => {
+  const liNode = e.target.parentNode;
+  liNode.remove();
+};
+
+const createLI = (task, button) => {
+  const listNode = document.createElement("li");
+  listNode.textContent = task;
+  listNode.append(button);
+  return listNode;
+};
+
+const createButton = () => {
+  const buttonNode = document.createElement("button");
+  buttonNode.textContent = "X";
+  buttonNode.id = "delete-button";
+  buttonNode.addEventListener("click", deleteTask);
+  return buttonNode;
+};
